b122a8c0a4204114638b5042a99817a26a737a71
DavideColabove <colabove.davide@gmail.com>
2026-07-26 17:02:27 +0200

    First version Telemetry.py

---
+++ Telemetry.py
@@ -0,0 +1,46 @@
+import requests
+
+def get_driver_laps(session_key, driver_number):
+    url = "https://api.openf1.org/v1/laps"
+
+    parameter = { "session_key": session_key, "driver_number": driver_number}
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    print(f"\nRecupero i tempi per il pilota {driver_number} nella sessione {session_key}...")
+
+    try:
+        response = requests.get(url, params=parameter, headers=headers)
+
+        if response.status_code == 401:
+            print("\nErrore 401: Il server continua a bloccarci. Potrebbe essere necessario usare un'altra session_key.")
+            print(f"Dettagli dal server: {response.text}")
+            return
+
+        response.raise_for_status()
+
+        laps_data = response.json()
+
+        if not laps_data:
+            print(f"\nNessun dato trovato con questi parametri. Verifica l'ID sessione!")
+            return
+
+        print(f"\nTrovati {len(laps_data)} giri. Ecco i primi 5:\n")
+        print("-" *50)
+
+        for lap in laps_data[:5]:
+            lap_number = lap.get("lap_number", "N/D")
+            lap_seconds = lap.get("lap_duration","N/D")
+
+            print(f"Giro: {lap_number} | Tempo: {lap_seconds} secondi")
+
+        print("-" *50)
+        print("Script completato con successo!")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Errore di comunicazione con le API: {e}")
+
+if __name__ == "__main__":
+    get_driver_laps(session_key=9159, driver_number=55)
