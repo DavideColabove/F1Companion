62982483863204ed30c3f725c10e2f50fac982eb
DavideColabove <colabove.davide@gmail.com>
2026-07-26 17:14:58 +0200

    Implemented search_max_speed(session_key, driver_number) function

--- Telemetry.py
+++ Telemetry.py
@@ -42,7 +42,55 @@ def get_driver_laps(session_key, driver_number):
     except requests.exceptions.RequestException as e:
         print(f"Errore di comunicazione con le API: {e}")
 
+def search_max_speed(session_key, driver_number): 
+    url = "https://api.openf1.org/v1/car_data"
 
+    parameters = {"session_key": session_key, "driver_number": driver_number}
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    print(f"\nAnalizzo la telemetria per il pilota {driver_number} nella sessione {session_key}...")
+
+    try: 
+        response = requests.get(url, params=parameters, headers=headers)
+
+        if response.status_code==401:
+            print("Errore 401: Sessione Live in corso. Riprova a fine gara!")
+            return
+
+        response.raise_for_status()
+        telemetry_data = response.json()
+
+        if not telemetry_data:
+            print("Nessun dato di telemetria!")
+            return
+
+        max_speed = 0
+        peak_details = None
+
+        for packet in telemetry_data:
+            current_speed = packet.get("speed", 0)
+
+            if current_speed > max_speed:
+                max_speed = current_speed
+                peak_details = packet
+
+        print("-" *50)
+        print(f"Velocità MASSIMA registrata: {max_speed} km/h")
+        print("-" *50)
+        print("Dettagli dell'auto in quel momento esatto:")
+        print(f"1. Marcia inserita: {peak_details.get('n_gear')}")
+        print(f"2. Giri motore (RPM): {peak_details.get('rpm')}")
+        print(f"3. Acceleratore: {peak_details.get('throttle')}%")
+        print(f"4. Freno premuto: {'Sì' if peak_details.get('brake') > 0 else 'No'}")
+        print(f"• DRS Aperto: {'Sì' if peak_details.get('drs') in [10, 12, 14] else 'No'}")
+        print("-" * 50)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Errore di comunicazione con le API: {e}")
 
 if __name__ == "__main__":
     get_driver_laps(session_key=9159, driver_number=55)
+    search_max_speed(session_key=9159, driver_number=55)
