8ded4c2304981c046eda9abb2ba2ff9715b279d8
DavideColabove <colabove.davide@gmail.com>
2026-07-29 18:01:28 +0200

    Added: threading support for simulate_dashboard, simulate_location e analyze_weather

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import threading
 from Telemetry import (
     get_driver_laps, 
     search_max_speed, 
@@ -10,13 +11,20 @@ from Telemetry import (
 def main():
     session = 9159
     pilot = 55
+
+    telemetry_thread = threading.Thread(target=simulate_dashboard, args=(session,pilot))
+    position_thread = threading.Thread(target=simulate_location, args=(session,pilot))
+    weather_thread = threading.Thread(target=analyze_weather, args=(session,))
     
-    # get_driver_laps(session_key=session, driver_number=pilot)
-    # search_max_speed(session_key=session, driver_number=pilot)
-    # analyze_pit_stop(session_key=session, driver_number=pilot)
+    get_driver_laps(session_key=session, driver_number=pilot)
+    search_max_speed(session_key=session, driver_number=pilot)
+    analyze_pit_stop(session_key=session, driver_number=pilot)
     # simulate_dashboard(session_key=session, driver_number=pilot)
-    simulate_location(session_key=session, driver_number=pilot)
-    # analyze_weather(session_key=sessione)
+    # simulate_location(session_key=session, driver_number=pilot)
+    # analyze_weather(session_key=session)
+    telemetry_thread.start()
+    position_thread.start()
+    weather_thread.start()
 
 if __name__ == "__main__":
     try:
